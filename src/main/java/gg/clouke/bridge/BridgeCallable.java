c328c09a9087858b68429e0d14a9fdb094a507ce
Clouke <76826179+Clouke@users.noreply.github.com>
2022-05-24 18:50:19 +0200

    changed `request();` to `onRequest();`

--- src/main/java/gg/clouke/bridge/BridgeCallable.java
+++ src/main/java/gg/clouke/bridge/BridgeCallable.java
@@ -1,7 +1,6 @@
 package gg.clouke.bridge;
 
-import gg.clouke.bridge.relay.BridgeRequest;
-import gg.clouke.bridge.relay.Packet;
+import gg.clouke.bridge.provider.BridgeRequest;
 
 /**
  * @author Clouke
@@ -15,13 +14,13 @@ public interface BridgeCallable {
      * @param packet {@link Packet}
      * @param request {@link BridgeRequest}
      */
-    public void request(Packet packet, BridgeRequest request);
+    void onRequest(Packet packet, BridgeRequest request);
 
     /**
      * Get the annotation information
      * @return Returns the {@link Identifier} from the subbed class
      */
-    public default Identifier get() {
+    default Identifier get() {
         return this.getClass().getAnnotation(Identifier.class);
     }
 
@@ -29,7 +28,7 @@ public interface BridgeCallable {
      * Get the packet from {@link Identifier}
      * @return Returns the direct {@link Packet} type
      */
-    public default Packet type() {
+    default Packet type() {
         return this.get().type();
     }
 
